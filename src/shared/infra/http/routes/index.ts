1b1c9ff4d5cacd188974632446f735c0d2691bd7
Matheus Costa <costamatheus97@live.com>
2021-10-20 21:42:19 -0300

    fix: removed unused routes from template

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,18 +1,14 @@
 import { Router } from "express";
 
 import authenticate from "./authenticate";
-import cars from "./cars";
 import categories from "./categories";
-import rentals from "./rentals";
-import specifications from "./specifications";
+import products from "./products";
 import users from "./users";
 
 const router = Router();
 
-router.use("/cars", cars);
 router.use("/categories", categories);
-router.use("/rentals", rentals);
-router.use("/specifications", specifications);
+router.use("/products", products);
 router.use("/users", users);
 router.use(authenticate);
 
